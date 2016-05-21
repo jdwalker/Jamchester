df3f6118323aade20b77d83861bc31bc7687a2f2
James Walker <jdwalker08@gmail.com>
2016-05-21 15:39:48 +0100

    rest of changes + prefab

---
+++ Jamchester2016/Assets/_Scripts/RayCast.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class RayCast : MonoBehaviour {
+
+	private Camera _camera;
+
+	public int GuiAimSize = 12;
+	public int _rayDistance = 200;
+	public string CurrentTag = string.Empty;
+
+	// Use this for initialization
+	void Start () {
+		_camera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool transform = Input.GetButtonDown("Transform");
+		bool capture = Input.GetButtonDown("Capture");
+
+		if(transform ^ capture)
+		{
+			Vector3 point = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
+			Ray ray = _camera.ScreenPointToRay(point);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit))
+			{
+				
+			}
+		}
+	}
+
+	void OnGUI()
+	{
+		float posX = _camera.pixelWidth / 2 - GuiAimSize / 4;
+		float posY = _camera.pixelHeight / 2 - GuiAimSize / 4;
+		GUI.Label(new Rect(posX, posY, GuiAimSize, GuiAimSize), "*");
+	}
+}
